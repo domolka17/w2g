ca20a11b01dac4e775c9550c811049cddd84c25a
domolka17 <domenik.roessner@gmail.com>
2022-09-27 18:51:07 +0200

    Hashrouter push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { useEffect } from "react";
 import './App.css';
 import './components/css/navbar.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Startseite';
 import About from './components/about';
 import Help from './components/Help';
 import Room from './components/Room';
-import Host from "./components/Host";
 import Watchparty from "./components/Watchparty";
 import { leaveRoom } from "./components/Controller/RoomController";
 import UserCreatrSide from "./components/UserCreateSide";
@@ -40,23 +39,23 @@ const logOut = (id)=>{
     method:'DELETE', headers:{"Content-Type": "application/json"}
   }).then(sessionStorage.clear())
 }
-  return (
-    <Router >
-      <Navbar />
+return (
+  <HashRouter>
+    <Navbar />
       <Routes>
         <Route exact path='/' element={<Home />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/index' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/Help' element={<Help />} />
-        <Route path="/Room" element={<Room/>}/>
-        <Route path="/Watchparty/:roomid" element={<Watchparty/>}/>
-        <Route path="/UserCreateSide" element={<UserCreatrSide/>}/>
-        <Route path="/Chat" element={<Chat/>}/>
-        <Route path="*" element={<NotFound/> }/>     
+        <Route exact path='/index' element={<Home />} />
+        <Route exact path='/about' element={<About />} />
+        <Route exact path='/Help' element={<Help />} />
+        <Route exact path="/Room" element={<Room />} />
+        <Route exact path="/Host" element={<Host />} />
+        <Route exact path="/Watchparty/:roomid" element={<Watchparty />} />
+        <Route exact path="/UserCreateSide" element={<UserCreatrSide />} />
+        <Route exact path="/Chat" element={<Chat />} />
+        <Route exact path="*" element={<NotFound />} />
       </Routes>
-    </Router>
-  );
+  </HashRouter >
+);
 }
 
 export default App;
